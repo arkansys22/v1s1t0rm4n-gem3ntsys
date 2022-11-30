e478ab5de8216cc6acd39c01c9659c00a3f67688
arkans <82564511+arkansys22@users.noreply.github.com>
2022-11-30 08:37:32 +0700

    up

--- application/views/fronts/css.php
+++
@@ -1,8 +0,0 @@
-<link href="<?php echo base_url()?>assets/frontend/theme/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="<?php echo base_url()?>assets/frontend/theme/css/jquery-ui.structure.min.css" rel="stylesheet" type="text/css"/>
-<link href="<?php echo base_url()?>assets/frontend/theme/css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/theme/css/font-awesome.min.css">
-<link href="<?php echo base_url()?>assets/frontend/theme/css/style.css" rel="stylesheet" type="text/css"/>
-<link rel="alternate" href="<?php echo base_url()?>" hreflang="id" />
-<link href='<?php echo base_url()?>' rel='canonical'/>
-<!-- Google Analytics -->
